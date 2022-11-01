b05ae7b3b5692a9397cb7cabbddcfdabdf4809dc
Syed-Kabir-Rifae <68635020+Syed-Kabir-Rifae@users.noreply.github.com>
2022-11-01 11:18:26 +0530

    Add files via upload

---
+++ TestExcel/src/test/java/com/ibm/TestExcel/TestExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.TestExcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestExcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
